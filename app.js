d9b1353247e8fa58892f977c7ad399aa67f39276
suhailakhtar039 <suhailakhtar039@gmail.com>
2020-08-05 12:47:25 +0530

    updated app.js

--- app.js
+++ app.js
@@ -83,4 +83,6 @@ app.use('/stories',require('./routes/stories'))
 
 const PORT=process.env.PORT || 3000
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
